163858f4d683608b680fa1b3e99147ba87f590ee
Jon Gilkison <jon.gilkison@gmail.com>
2017-09-16 08:20:15 +0700

    v5.5.2

---
+++ src/EntryNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ILAB_Illuminate\Container;
+
+use Exception;
+use Psr\Container\NotFoundExceptionInterface;
+
+class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
+{
+    //
+}
